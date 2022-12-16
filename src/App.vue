0a615e0d6d4ae8b4da1da3347ba809c89c6188a1
YusufGND <yusufgundogdu112@gmail.com>
2022-12-16 23:03:21 +0300

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+<transition-group name="slideContainer" mode="out-in">
+<component 
+key="mainComponent"
+@activeComponentEvent = "activeComponent =  $event"
+:is="activeComponent"
+></component>
+<canvas key="canvas" id="canvas" v-show="activeComponent == 'app-celeb'"></canvas>
+</transition-group>
+</template>
+<script>
+
+import GameCards from "./components/GameCards";
+import Lose from "./components/Lose";
+import celeb from "./components/celeb";
+export default {
+    data(){
+       return{ 
+           activeComponent: "app-game-cards"
+       }
+    },
+    components:{
+        appGameCards : GameCards,
+        appLose : Lose,
+        appCeleb : celeb,
+        
+    }
+}
+</script>
+<style>
+body{
+    font-family: sans-serif;
+}
+.slideContainer-enter{}
+.slideContainer-enter-active
+{
+    animation: slide-in .3s ease-out forwards;
+}
+.slideContainer-leave{}
+.slideContainer-leave-active
+{
+    animation: slide-out .3s ease-out forwards;
+}
+
+@keyframes slide-in {
+    from
+    {
+        transform: translateX(-1000px);
+        opacity: 0;
+    }
+
+    to  
+    {
+        transform: translateX(0px);
+        opacity: 1;
+    }
+}
+@keyframes slide-out {
+    from{
+        transform: translateX(0px);
+    }
+    to  
+    {
+        transform: translateX(1000px);
+    }
+}
+</style>
---
+++ src/components/DefaultCard.vue
@@ -0,0 +1,38 @@
+<template>
+     <div class="card">
+              
+           </div>
+</template> 
+<script>
+
+export default {
+    
+}
+</script>
+
+<style scoped>
+    
+ .card
+{
+    width: 200px;
+    height: 232px;
+    border-radius: 5px;
+    border:1px  solid #666;
+    transition: box-shadow  .5s;
+    margin-left: 15px;
+    background-image: url("../assets/vue-js-egitim-background.jpg");
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+}
+.card:hover
+{
+    box-shadow: 0px 5px 48px #666;
+    transition: box-shadow .5s;
+}
+.card img
+{
+    width: 100%;
+    height: 100%;
+} 
+</style>
